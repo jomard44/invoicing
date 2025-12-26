22d13b3a9cf7d9403ef0b91d0d679a3090a45d9e
jomard <jomrdali44@gmail.com>
2025-12-26 17:20:59 -0400

    hashed passwords

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,36 +1,40 @@
-import express from "express"
-import User from "../../models/User.js"
+import express from "express";
+import User from "../../models/User.js";
+import bcrypt, { genSalt } from "bcrypt";
 
-const auth = express()
+const auth = express();
 
-auth.post("/register",async(req,res)=>{
-    try{
-        const {email, password} = req.body
-        const user = await User.findOne({email})
-        if(user){
-            res.status(400).json({message:"User already exist"})
-        }
-        const newUser = await User.create({email,password})
-        res.status(201).json({message:"User has been created"})
-    }catch(error){
-        res.status(400).json({message:"Invalid credentials"})
+auth.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).json({ message: "User already exist" });
     }
-    
 
-})
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword });
+    res.status(201).json({ message: "User has been created" });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid credentials" });
+  }
+});
 
-auth.post("/login",async (req,res)=>{
-    try{
-
-        const {email, password} = req.body
-        const user = await User.findOne({email})
-        if(!user){
-            res.status(400).json({message:"User does not exist"})
-        }
-        res.status(200).json({message:"You are now loged in"})
-    }catch(error){
-        res.status(400).json({message:"Invalid credentials"})
+auth.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User does not exist" });
     }
-})
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      res.status(400).json({ message: "Wrong credentials" });
+    }
+    res.status(200).json({ message: "You are now loged in" });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid credentials" });
+  }
+});
 
-export default auth
+export default auth;
